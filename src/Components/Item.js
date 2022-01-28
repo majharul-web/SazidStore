18cb97dcf5e1925886ace963058aadd8fbd76742
majharul <majharuljony163@gmail.com>
2022-01-28 22:41:23 +0600

    show item

---
+++ src/Components/Item.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+const Item = ({ item }) => {
+    const { strCategory, strCategoryThumb, strCategoryDescription } = item;
+    return (
+        <View style={{
+
+            flex: 0.5,
+            borderRadius: 20,
+            backgroundColor: '#FFFFFF',
+            minHeight: 150,
+            padding: 20
+        }}>
+            <Image
+                resizeMode='contain'
+                style={{ width: 100, height: 100 }}
+                source={{ uri: strCategoryThumb }}
+            />
+            <Text style={{ color: 'black', fontSize: 20, fontWeight: 'bold' }}>{strCategory}</Text>
+            <Text style={{ color: 'gray', fontSize: 16 }}>{strCategory}</Text>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginVertical: 10, }}>
+                <Text style={{ color: 'black', fontSize: 18, fontWeight: 'bold' }}>$ 14</Text>
+                <Pressable>
+                    <MaterialIcons name="favorite" color={'black'} size={20} />
+                </Pressable>
+            </View>
+        </View>
+    );
+};
+
+export default Item;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,10 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { FlatList, Image, ImageBackground, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { FlatGrid } from 'react-native-super-grid';
+import Item from '../Components/Item';
 
 
 
 const Home = () => {
+
+    const [meals, setMeals] = useState([]);
+    useEffect(() => {
+        fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+            .then(res => res.json())
+            .then(data => setMeals(data.categories));
+    }, []);
 
     return (
         <View style={styles.body}>
@@ -26,7 +35,7 @@ const Home = () => {
 
                     <View style={{ width: '77%', justifyContent: 'center' }}>
                         <TextInput
-                            style={{ backgroundColor: '#F9F9F9', height: '60%', borderRadius: 15, padding: 5 ,color:'black'}}
+                            style={{ backgroundColor: '#F9F9F9', height: '60%', borderRadius: 15, padding: 5, color: 'black' }}
                         >
                         </TextInput>
                         <MaterialIcons style={{ position: 'absolute', right: 15 }} name="search" color={'black'} size={20} />
@@ -42,7 +51,18 @@ const Home = () => {
 
             </View>
 
-            <View style={{ backgroundColor: 'black', flex: 3 }}></View>
+            <View style={{ flex: 3 }}>
+                <Text style={{color:'black',fontSize:20,fontWeight:'bold',marginVertical:5,textAlign:'center'}}>Found {meals.length} Results</Text>
+                <FlatGrid
+                    keyExtractor={item => item.idCategory}
+                    data={meals}
+                    style={{ flex: 1 }}
+                    spacing={20}
+                    renderItem={({ item }) => (
+                        <Item item={item}></Item>
+                    )}
+                />
+            </View>
         </View >
     );
 };
